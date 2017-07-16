c614ed0b55b1a6b39fb5f0c6e61e7b65b00dfb07
liuyuanyang <liuyuanyang@weidian.com>
2017-07-16 19:43:17 +0800

    update

---
+++ src/snippets/dynamic-routes/routes.js
@@ -0,0 +1,13 @@
+
+import doc_bbc3f6dc4338ea87939b4413f02a92fb from './bbc3f6dc4338ea87939b4413f02a92fb.vue';
+import doc_e7b3677039a8cf63c66645ba246fc4d6 from './e7b3677039a8cf63c66645ba246fc4d6.vue';
+
+module.exports = [
+{
+              path: '/JavaScript/基础知识',
+              component: doc_bbc3f6dc4338ea87939b4413f02a92fb
+            },{
+              path: '/JavaScript/基础知识0',
+              component: doc_e7b3677039a8cf63c66645ba246fc4d6
+            }
+];
--- src/snippets/map.js
+++
@@ -1,7 +0,0 @@
-import AA from './routes/articals/JavaScript/基础知识.md';
-
-
-module.exports = [{
-    path: '/JavaScript/基础知识',
-    component: AA
-  }];
