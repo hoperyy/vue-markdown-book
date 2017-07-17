fba8d905c219e4452480adfbf9182c0c410b647e
hoperyy <antfootmail@gmail.com>
2017-07-18 00:23:21 +0800

    update

---
+++ src/snippets/components/Level2Nav.vue
@@ -0,0 +1,34 @@
+<template>
+    <ul class="level1">
+        <li class="level1-item" v-for="(item, index) in arr">
+            <ul class="level2">
+                <li class="level2-item" v-if="item.children" v-for="(level2Item, level2Index) in item.children">
+                    <div>{{level2Item.path}}</div>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</template>
+
+<script>
+
+export default {
+    components: {
+    },
+    props: ['arr']
+};
+
+</script>
+
+<style lang="less" scoped>
+ul, li {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+}
+.level1 {
+  float: left;
+  padding: 10px;
+  border: 1px solid black;
+}
+</style>
