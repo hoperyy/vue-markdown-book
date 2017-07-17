70990dc438bf34ce2ee6b8130bf329dc81aa6350
hoperyy <antfootmail@gmail.com>
2017-07-17 23:46:27 +0800

    update

--- src/snippets/dynamic-files/routes.js
+++ src/snippets/dynamic-files/routes.js
@@ -1,6 +1,6 @@
 
-import doc_1014efde1ae9f76be82e7de52575da5e from './1014efde1ae9f76be82e7de52575da5e.vue';
-import doc_d6fd6e49b8815387e66e1cbc245ba9c9 from './d6fd6e49b8815387e66e1cbc245ba9c9.vue';
+import doc_202d628e418ec365cd4dd9eac7b10169 from './202d628e418ec365cd4dd9eac7b10169.vue';
+import doc_18cc383022daa97a180d4d504672506c from './18cc383022daa97a180d4d504672506c.vue';
 import doc_bbc3f6dc4338ea87939b4413f02a92fb from './bbc3f6dc4338ea87939b4413f02a92fb.vue';
 import doc_e7b3677039a8cf63c66645ba246fc4d6 from './e7b3677039a8cf63c66645ba246fc4d6.vue';
 import doc_a534fa75f29ac8e9a79d1679d3fc52b0 from './a534fa75f29ac8e9a79d1679d3fc52b0.vue';
@@ -8,11 +8,11 @@ import doc_b9a5d6440c713c705a9540f8ed11c061 from './b9a5d6440c713c705a9540f8ed11
 
 module.exports = [
 {
-              path: '/JavaScript/JavaScript/基础知识',
-              component: doc_1014efde1ae9f76be82e7de52575da5e
+              path: '/JavaScript/AA/基础知识',
+              component: doc_202d628e418ec365cd4dd9eac7b10169
             },{
-              path: '/JavaScript/JavaScript/基础知识0',
-              component: doc_d6fd6e49b8815387e66e1cbc245ba9c9
+              path: '/JavaScript/AA/基础知识0',
+              component: doc_18cc383022daa97a180d4d504672506c
             },{
               path: '/JavaScript/基础知识',
               component: doc_bbc3f6dc4338ea87939b4413f02a92fb
