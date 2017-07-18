a90ed893529c17f568fdfd744f03f21222d50bc3
liuyuanyang <liuyuanyang@weidian.com>
2017-07-18 23:06:38 +0800

    update

--- src/snippets/components/Level2Nav.vue
+++ src/snippets/components/Level2Nav.vue
@@ -2,7 +2,7 @@
     <ul class="level1">
         <li v-bind:class="getLevel1Class(level1Item, level1Index)" v-for="(level1Item, level1Index) in arr">
             <ul class="level2">
-                <li @click="click(level2Index)" v-bind:class="getLevel2Class(level2Item, level2Index)" v-if="level1Item.children" v-for="(level2Item, level2Index) in level1Item.children">
+                <li @click="click(level2Index)" v-bind:class="getLevel2Class(level2Item, level2Index, level1Index)" v-if="level1Item.children" v-for="(level2Item, level2Index) in level1Item.children">
                     <div>{{level2Item.path.split('/').pop()}}</div>
                 </li>
             </ul>
@@ -29,11 +29,11 @@ export default {
         'current': this.currentIndex[0] + '' === level1Index + ''
       };
     },
-    getLevel2Class(level2Item, level2Index) {
+    getLevel2Class(level2Item, level2Index, level1Index) {
       return {
         'level2-item': true,
         'is-folder': level2Item.type === 'directory',
-        'current': this.currentIndex[1] + '' === level2Index + ''
+        'current': [this.currentIndex[0], this.currentIndex[1]].join('-') === [level1Index, level2Index].join('-')
       };
     }
   }
