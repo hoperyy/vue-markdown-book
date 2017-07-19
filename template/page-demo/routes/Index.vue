bf2531734a3dab2c81c57affb99ef4c9b517caa4
liuyuanyang <liuyuanyang@weidian.com>
2017-07-19 21:17:14 +0800

    update

--- template/page-demo/routes/Index.vue
+++ template/page-demo/routes/Index.vue
@@ -1,31 +1,27 @@
+
 <template>
     <div class="hoper-body">
         <Mheader></Mheader>
-        <Mmenu></Mmenu>
+        <div class="hoper-content">
+            <Mmenu :currentIndex="['0']"></Mmenu>
+            <div class="hoper-doc">
+            </div>
+        </div>
         <Mfooter></Mfooter>
     </div>
 </template>
 
 <script>
-
 import Mheader from '../../components/Header.vue';
 import Mfooter from '../../components/Footer.vue';
-import Mmenu from '../components/Menu.vue'
+import Mmenu from '../components/Menu.vue';
 
 export default {
     components: {
         Mheader,
         Mfooter,
         Mmenu
-    },
-    data() {
-        return {
-            message: 'Hello World'
-        };
     }
 };
 
 </script>
-
-<style lang="less">
-</style>
