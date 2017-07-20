cbefd981fc0aab9eeb6ffc6a2c008846d5d65c80
hoperyy <antfootmail@gmail.com>
2017-07-20 21:14:51 +0800

    update

---
+++ docs/snippets/Chrome-Plugin/template/popup.html.md
@@ -0,0 +1,15 @@
+```
+<!DOCTYPE HTML>
+<html lang="zh-CN">
+    <head>
+        <title>弹出页面</title>
+        <meta charset="utf-8">
+		<link rel="stylesheet" href="../static/css/popup.css" />
+		<script type="text/javascript" src="../static/lib/jquery-3.1.1.min.js"></script>
+		<script type="text/javascript" src="../static/js/popup.js"></script>
+    </head>
+    <body>
+      	<div>demo</div>
+    </body>
+</html>
+```
--- src/snippets/dynamic-files/routes.js
+++ src/snippets/dynamic-files/routes.js
@@ -1,5 +1,17 @@
 
+import doc_3f9469fad8f73306831d13488fa55947 from './3f9469fad8f73306831d13488fa55947.vue';
+import doc_70253488c95e15a16ca30d6af8c15af4 from './70253488c95e15a16ca30d6af8c15af4.vue';
+import doc_8268bb1589cd0a1172c167414f9f0d9b from './8268bb1589cd0a1172c167414f9f0d9b.vue';
 
 module.exports = [
-
+{
+              path: '/Chrome-Plugin/README',
+              component: doc_3f9469fad8f73306831d13488fa55947
+            },{
+              path: '/Chrome-Plugin/manifest.json',
+              component: doc_70253488c95e15a16ca30d6af8c15af4
+            },{
+              path: '/Chrome-Plugin/template/popup.html',
+              component: doc_8268bb1589cd0a1172c167414f9f0d9b
+            }
 ];
