8c0bb541e079a61f73c3bb109a30282de961fc22
liuyuanyang <liuyuanyang@weidian.com>
2017-07-20 23:47:47 +0800

    update

--- src/snippets/dynamic-files/routes.js
+++ src/snippets/dynamic-files/routes.js
@@ -1,15 +1,39 @@
 
+import doc_1f1cc5b67dda6e4fd898b28f1d071cf4 from './1f1cc5b67dda6e4fd898b28f1d071cf4.vue';
+import doc_827c47de985e8d883e53fe9ffb341087 from './827c47de985e8d883e53fe9ffb341087.vue';
 import doc_3f9469fad8f73306831d13488fa55947 from './3f9469fad8f73306831d13488fa55947.vue';
 import doc_70253488c95e15a16ca30d6af8c15af4 from './70253488c95e15a16ca30d6af8c15af4.vue';
+import doc_4bc5cf3b306fd4fe5248b781bd6661f3 from './4bc5cf3b306fd4fe5248b781bd6661f3.vue';
+import doc_40a99629735d7cd82db016d06482caff from './40a99629735d7cd82db016d06482caff.vue';
+import doc_251d480a48c275dac0f2f40d0dae960a from './251d480a48c275dac0f2f40d0dae960a.vue';
+import doc_de746f5892cf07a98f18e9b89aa23ac3 from './de746f5892cf07a98f18e9b89aa23ac3.vue';
 import doc_8268bb1589cd0a1172c167414f9f0d9b from './8268bb1589cd0a1172c167414f9f0d9b.vue';
 
 module.exports = [
 {
+              path: '/Chrome-Plugin',
+              component: doc_1f1cc5b67dda6e4fd898b28f1d071cf4
+            },{
+              path: '/JavaScript',
+              component: doc_827c47de985e8d883e53fe9ffb341087
+            },{
               path: '/Chrome-Plugin/README',
               component: doc_3f9469fad8f73306831d13488fa55947
             },{
               path: '/Chrome-Plugin/manifest.json',
               component: doc_70253488c95e15a16ca30d6af8c15af4
+            },{
+              path: '/Chrome-Plugin/static',
+              component: doc_4bc5cf3b306fd4fe5248b781bd6661f3
+            },{
+              path: '/Chrome-Plugin/template',
+              component: doc_40a99629735d7cd82db016d06482caff
+            },{
+              path: '/Chrome-Plugin/static/css',
+              component: doc_251d480a48c275dac0f2f40d0dae960a
+            },{
+              path: '/Chrome-Plugin/static/img',
+              component: doc_de746f5892cf07a98f18e9b89aa23ac3
             },{
               path: '/Chrome-Plugin/template/popup.html',
               component: doc_8268bb1589cd0a1172c167414f9f0d9b
