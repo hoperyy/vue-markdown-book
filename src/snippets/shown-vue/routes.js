fb48f09ac7830b272c9fef65656a289620bceaba
liuyuanyang <liuyuanyang@weidian.com>
2017-07-24 10:37:34 +0800

    update readme

--- src/snippets/shown-vue/routes.js
+++ src/snippets/shown-vue/routes.js
@@ -1,41 +1,29 @@
 
-import doc_c717d1d8c144924ce3aa4d577c48e52b from './c717d1d8c144924ce3aa4d577c48e52b.vue';
-import doc_71ddd67ef55043f8b9ccab0f6f50458d from './71ddd67ef55043f8b9ccab0f6f50458d.vue';
-import doc_131cd7ca2f76fe1c90ca25ec9009630e from './131cd7ca2f76fe1c90ca25ec9009630e.vue';
-import doc_c8390ac623ce8d49559dd9be5bf955f8 from './c8390ac623ce8d49559dd9be5bf955f8.vue';
-import doc_6aa4de1cbad2e35744579363367890bf from './6aa4de1cbad2e35744579363367890bf.vue';
-import doc_ebb6fb540b6e10dff88c4c8944f064a9 from './ebb6fb540b6e10dff88c4c8944f064a9.vue';
-import doc_4e9a2878dc6d10bc9e64d38bf6cb7b87 from './4e9a2878dc6d10bc9e64d38bf6cb7b87.vue';
-import doc_2c40f5c42b05b1f2bd3e65865a6367ba from './2c40f5c42b05b1f2bd3e65865a6367ba.vue';
-import doc_e8aea3af6873cb418c8e893a1003820d from './e8aea3af6873cb418c8e893a1003820d.vue';
+import doc_79c177649a76b2581fd48ce9ae96056e from './79c177649a76b2581fd48ce9ae96056e.vue';
+import doc_1a3fd5d1dc9b27f706ef29e00d5e5335 from './1a3fd5d1dc9b27f706ef29e00d5e5335.vue';
+import doc_e5d6e49ff68871fb9725a641ab7c6977 from './e5d6e49ff68871fb9725a641ab7c6977.vue';
+import doc_f7e70a754196651cb616bed67f8e8b58 from './f7e70a754196651cb616bed67f8e8b58.vue';
+import doc_25d6786b6d75ada59ce5a62489728efa from './25d6786b6d75ada59ce5a62489728efa.vue';
+import doc_65c38de4eae0a499b1e2f5f1113edb0a from './65c38de4eae0a499b1e2f5f1113edb0a.vue';
 
 module.exports = [
 {
           path: '/assets',
-          component: doc_c717d1d8c144924ce3aa4d577c48e52b
+          component: doc_79c177649a76b2581fd48ce9ae96056e
         },{
           path: '/assets/markdown-img-paste-20170721095758212.png',
-          component: doc_71ddd67ef55043f8b9ccab0f6f50458d
+          component: doc_1a3fd5d1dc9b27f706ef29e00d5e5335
         },{
           path: '/assets/markdown-img-paste-20170722233753643.png',
-          component: doc_131cd7ca2f76fe1c90ca25ec9009630e
+          component: doc_e5d6e49ff68871fb9725a641ab7c6977
         },{
           path: '/assets/markdown-img-paste-201707222337536430.png',
-          component: doc_c8390ac623ce8d49559dd9be5bf955f8
+          component: doc_f7e70a754196651cb616bed67f8e8b58
         },{
           path: '/test.js',
-          component: doc_6aa4de1cbad2e35744579363367890bf
+          component: doc_25d6786b6d75ada59ce5a62489728efa
         },{
           path: '/test.md',
-          component: doc_ebb6fb540b6e10dff88c4c8944f064a9
-        },{
-          path: '/test.py',
-          component: doc_4e9a2878dc6d10bc9e64d38bf6cb7b87
-        },{
-          path: '/test0.md',
-          component: doc_2c40f5c42b05b1f2bd3e65865a6367ba
-        },{
-          path: '/test1.md',
-          component: doc_e8aea3af6873cb418c8e893a1003820d
+          component: doc_65c38de4eae0a499b1e2f5f1113edb0a
         }
 ];
