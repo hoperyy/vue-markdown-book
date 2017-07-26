d95e356d8bef6e98a8cc781395e06c0fa468fc04
liuyuanyang <liuyuanyang@weidian.com>
2017-07-26 09:39:14 +0800

    update

---
+++ docs/demo/test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>vue 方式插入的 dom</div>
+</template>
+
+<style lang="less" scoped>
+div {
+  background: black;
+  text-align: center;
+  color: white;
+  padding: 10px 0;
+}
+</style>
+
+<script>
+
+export default {
+  components: {
+
+  }
+}
+</script>
