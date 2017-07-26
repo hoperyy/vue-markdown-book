147b968c6210c6fef5e09dcf9318e0d74e949329
hoperyy <antfootmail@gmail.com>
2017-07-26 23:36:54 +0800

    .md 后缀名隐藏

--- src/demo/components/Level3Nav.vue
+++ src/demo/components/Level3Nav.vue
@@ -5,8 +5,8 @@
                 <li v-if="!level2Item.shouldNotShow" v-bind:class="getLevel2Class(level2Item, level2Index, level1Index)" v-for="(level2Item, level2Index) in level1Item.children">
                     <ul class="level3" v-if="level2Item.children">
                         <li v-if="!level3Item.shouldNotShow" @click="click(level3Index)" v-bind:class="getLevel3Class(level3Item, level3Index, level2Index, level1Index)" v-for="(level3Item, level3Index) in level2Item.children">
-                          <div v-if="level3Item.type === 'directory'">{{level3Item.path.split('/').pop()}}</div>
-                          <router-link :to="level3Item.path" v-if="level3Item.type !== 'directory'">{{level3Item.path.split('/').pop()}}</router-link>
+                          <div v-if="level3Item.type === 'directory'">{{level3Item.path.split('/').pop().replace(/\.md$/, '')}}</div>
+                          <router-link :to="level3Item.path" v-if="level3Item.type !== 'directory'">{{level3Item.path.split('/').pop().replace(/\.md$/, '')}}</router-link>
                         </li>
                     </ul>
                 </li>
