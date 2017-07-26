6fe33610c527d9466d488c66b0904c8085b96f59
liuyuanyang <liuyuanyang@weidian.com>
2017-07-26 08:34:45 +0800

    update

---
+++ src/demo/shown-vue/c9ff962cb8d3115f1ceb7bb4a29ab897.vue
@@ -0,0 +1,32 @@
+
+          <template>
+              <div class="hoper-body">
+
+                  <div class="hoper-content">
+                      <Mmenu :currentIndex="['1']"></Mmenu>
+                      <div class="hoper-doc">
+                        <Doc></Doc>
+                      </div>
+                  </div>
+
+              </div>
+          </template>
+
+          <script>
+          import Mheader from '../../components/Header.vue';
+          import Mfooter from '../../components/Footer.vue';
+          import Mmenu from '../components/Menu.vue';
+
+          import Doc from './loaded-doc/index.html.md';
+
+          export default {
+              components: {
+                  Mheader,
+                  Mfooter,
+                  Mmenu,
+                  Doc
+              }
+          };
+
+          </script>
+        
---
+++ src/demo/shown-vue/routes.js
@@ -0,0 +1,29 @@
+
+import doc_82c3bdb2df8785a2957bfb53b809adbe from './82c3bdb2df8785a2957bfb53b809adbe.vue';
+import doc_03c275c786188c421a778233e9716ca4 from './03c275c786188c421a778233e9716ca4.vue';
+import doc_c9ff962cb8d3115f1ceb7bb4a29ab897 from './c9ff962cb8d3115f1ceb7bb4a29ab897.vue';
+import doc_0ae36df161f13187dad848636178e8fc from './0ae36df161f13187dad848636178e8fc.vue';
+import doc_d97f079e9b51cdeda9d789b435ae8d16 from './d97f079e9b51cdeda9d789b435ae8d16.vue';
+import doc_889a1b6187fb7c03a3c6f684e84c3995 from './889a1b6187fb7c03a3c6f684e84c3995.vue';
+
+module.exports = [
+{
+          path: '/assets',
+          component: doc_82c3bdb2df8785a2957bfb53b809adbe
+        },{
+          path: '/assets/markdown-img-paste-20170724185101208.png',
+          component: doc_03c275c786188c421a778233e9716ca4
+        },{
+          path: '/index.html',
+          component: doc_c9ff962cb8d3115f1ceb7bb4a29ab897
+        },{
+          path: '/index.js',
+          component: doc_0ae36df161f13187dad848636178e8fc
+        },{
+          path: '/test.md',
+          component: doc_d97f079e9b51cdeda9d789b435ae8d16
+        },{
+          path: '/test.vue',
+          component: doc_889a1b6187fb7c03a3c6f684e84c3995
+        }
+];
