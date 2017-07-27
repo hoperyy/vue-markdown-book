a29cc7ee27f6412ed9a09a6d36d4913ce5a9e90f
liuyuanyang <liuyuanyang@weidian.com>
2017-07-27 10:00:26 +0800

    新增用户配置目录

---
+++ book.config.js
@@ -0,0 +1,17 @@
+module.exports = () => {
+
+    const config = {
+
+        // build folder
+        buildFolder: require('path').join(__dirname, '../vue-markdown-book-ghpages'),
+
+        // theme
+        theme: 'default',
+
+        // user name
+        userName: 'xxx'
+
+    };
+
+    return config;
+};
