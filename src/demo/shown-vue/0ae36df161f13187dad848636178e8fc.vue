0cb0ce97330e35c5081c803a545ac69f36e13114
liuyuanyang <liuyuanyang@weidian.com>
2017-07-27 13:57:50 +0800

    完善对主题的开发方式支持

--- src/demo/shown-vue/0ae36df161f13187dad848636178e8fc.vue
+++ src/demo/shown-vue/0ae36df161f13187dad848636178e8fc.vue
@@ -1,31 +1,28 @@
+<template>
+    <div class="theme-default-body">
+        <div class="theme-default-content">
+            <Mmenu :currentIndex="['3']"></Mmenu>
+            <div class="common-theme-markdown-doc theme-default-markdown-doc">
+                <Doc></Doc>
+            </div>
+        </div>
+    </div>
+</template>
 
-                <template>
-                    <div class="theme-default-body">
-                        <div class="theme-default-content">
+<script>
+import Mheader from '../components/common/Header.vue';
+import Mfooter from '../components/common/Footer.vue';
+import Mmenu from '../components/Menu.vue';
 
-                            <Mmenu :currentIndex="['3']"></Mmenu>
-                            <div class="common-theme-markdown-doc theme-default-markdown-doc">
-                              <Doc></Doc>
-                            </div>
-                        </div>
-                    </div>
-                </template>
+import Doc from "./loaded-doc/index.js.md";
 
-                <script>
-                import Mheader from '../components/common/Header.vue';
-                import Mfooter from '../components/common/Footer.vue';
-                import Mmenu from '../components/Menu.vue';
+export default {
+    components: {
+        Mheader,
+        Mfooter,
+        Mmenu,
+        Doc
+    }
+};
 
-                import Doc from './loaded-doc/index.js.md';
-
-                export default {
-                    components: {
-                        Mheader,
-                        Mfooter,
-                        Mmenu,
-                        Doc
-                    }
-                };
-
-                </script>
-              
+</script>
--- theme-template/default/shown-vue-template/file-template.vue
+++ theme-template/default/shown-vue-template/file-template.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="theme-default-body">
         <div class="theme-default-content">
-            <Mmenu :currentIndex="['0']"></Mmenu>
-            <div class="common-theme-markdown-doc theme-default-markdown-doc"></div>
+            <Mmenu :currentIndex=$$_FILE_INDEX_$$></Mmenu>
+            <div class="common-theme-markdown-doc theme-default-markdown-doc">
+                <Doc></Doc>
+            </div>
         </div>
     </div>
 </template>
@@ -12,11 +14,14 @@ import Mheader from '../components/common/Header.vue';
 import Mfooter from '../components/common/Footer.vue';
 import Mmenu from '../components/Menu.vue';
 
+import Doc from $$_DOC_PATH_$$;
+
 export default {
     components: {
         Mheader,
         Mfooter,
         Mmenu,
+        Doc
     }
 };
 
