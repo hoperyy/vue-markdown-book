44b7158c18205a574246dacab30cf0789ab78894
liuyuanyang <liuyuanyang@weidian.com>
2017-07-30 22:38:09 +0800

    升级：每个文件夹可单独配置自己的主题

--- src/other/routes/5983f72af0a266f022bdb1f530c3b402.vue
+++ src/other/routes/5983f72af0a266f022bdb1f530c3b402.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="theme-default-body">
         <div class="theme-default-content">
-            <Mmenu :currentIndex="['1']"></Mmenu>
             <div class="common-theme-markdown-doc theme-default-markdown-doc">
                 <Doc></Doc>
             </div>
@@ -10,17 +9,11 @@
 </template>
 
 <script>
-import Mheader from '../components/common/Header.vue';
-import Mfooter from '../components/common/Footer.vue';
-import Mmenu from '../components/Menu.vue';
 
 import Doc from "./processed-doc/book.config.js.md";
 
 export default {
     components: {
-        Mheader,
-        Mfooter,
-        Mmenu,
         Doc
     }
 };
