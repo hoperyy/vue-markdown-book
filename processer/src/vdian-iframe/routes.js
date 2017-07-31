2ca3f3b8fcb7ccc74b37134eb80278913964db05
hoperyy <antfootmail@gmail.com>
2017-07-31 13:10:17 +0800

    update

---
+++ processer/src/vdian-iframe/routes.js
@@ -0,0 +1,11 @@
+import doc_6b9e3ccc4c2540859f5cf4dd73989206 from '../demo/routes/processed-doc/iframe-demo.vue.iframe-file.md';
+
+
+module.exports = [
+{
+  path: '/6b9e3ccc4c2540859f5cf4dd73989206',
+  component: doc_6b9e3ccc4c2540859f5cf4dd73989206
+},
+
+
+];
