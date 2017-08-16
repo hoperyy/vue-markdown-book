fb5de8371849d69141694334481395c9bdf37aeb
hoperyy <antfootmail@gmail.com>
2017-08-16 20:07:21 +0800

    主题美化

--- theme/h5-doc/routes-template/dir-template.vue
+++ theme/h5-doc/routes-template/dir-template.vue
@@ -1,23 +1,18 @@
 <template>
-    <div class="theme-vdianh5doc-body">
-        <Mheader></Mheader>
-        <div class="theme-vdianh5doc-content">
+    <div class="theme-h5-doc-body">
+        <div class="theme-h5-doc-content">
             <Mmenu :currentIndex=$$_FILE_INDEX_$$></Mmenu>
-            <div class="common-theme-markdown-doc theme-vdianh5doc-markdown-doc"></div>
+            <div class="common-theme-markdown-doc theme-h5-doc-markdown-doc"></div>
         </div>
     </div>
 </template>
 
 <script>
-import Mheader from '../components/common/Header.vue';
-import Mfooter from '../components/common/Footer.vue';
 import Mmenu from '../components/Menu.vue';
 
 export default {
     components: {
-        Mheader,
-        Mfooter,
-        Mmenu,
+        Mmenu
     }
 };
 
--- theme/h5-doc/routes-template/file-template.vue
+++ theme/h5-doc/routes-template/file-template.vue
@@ -1,9 +1,8 @@
 <template>
-    <div class="theme-vdianh5doc-body">
-        <Mheader></Mheader>
-        <div class="theme-vdianh5doc-content">
+    <div class="theme-h5-doc-body">
+        <div class="theme-h5-doc-content">
             <Mmenu :currentIndex=$$_FILE_INDEX_$$></Mmenu>
-            <div class="common-theme-markdown-doc theme-vdianh5doc-markdown-doc">
+            <div class="common-theme-markdown-doc theme-h5-doc-markdown-doc">
                 <Doc></Doc>
             </div>
         </div>
@@ -11,16 +10,12 @@
 </template>
 
 <script>
-import Mheader from '../components/common/Header.vue';
-import Mfooter from '../components/common/Footer.vue';
 import Mmenu from '../components/Menu.vue';
 
 import Doc from $$_DOC_PATH_$$;
 
 export default {
     components: {
-        Mheader,
-        Mfooter,
         Mmenu,
         Doc
     }
