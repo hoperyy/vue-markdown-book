c2c7f846871245ec022c24441caa4c43f4968b99
liuyuanyang <liuyuanyang@weidian.com>
2017-08-16 20:07:21 +0800

    主题美化

--- theme/h5-doc/routes/Index.vue
+++ theme/h5-doc/routes/Index.vue
@@ -1,9 +1,9 @@
 
 <template>
-    <div class="theme-vdianh5doc-body">
-        <div class="theme-vdianh5doc-content">
+    <div class="theme-h5-doc-body">
+        <div class="theme-h5-doc-content">
             <Mmenu :currentIndex="['0']"></Mmenu>
-            <div class="common-theme-markdown-doc theme-vdianh5doc-markdown-doc"></div>
+            <div class="common-theme-markdown-doc theme-h5-doc-markdown-doc"></div>
         </div>
     </div>
 </template>
